c1b9e57433c6ee759dfd30cef7797007d9224ca3
Jake Jarvis <jakejarvis@gmail.com>
2015-04-23 18:15:20 -0400

    initial HTML design

---
+++ app/assets/js/phood.js
@@ -0,0 +1,44 @@
+var myLat;
+var myLng;
+
+
+function init() {
+
+    var map_canvas = document.getElementById("map_canvas");
+    var tufts = new google.maps.LatLng(42.406484, -71.119023);
+    var options = {zoom: 15, center: tufts};
+
+    gmap = new google.maps.Map(map_canvas, options);
+
+
+    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+        navigator.geolocation.getCurrentPosition(function(position) {
+            myLat = position.coords.latitude;
+            myLng = position.coords.longitude;
+            renderMap();
+        });
+    } else {
+        alert("Please try a different browser.");
+    }
+    
+}
+
+function renderMap() {
+    me = new google.maps.LatLng(myLat, myLng);
+    gmap.panTo(me);
+
+    marker = new google.maps.Marker({
+        position: me,
+        title: "This is me, ",
+        icon: "/assets/img/carrot.png"
+    });
+    marker.setMap(gmap);
+
+    google.maps.event.addListener(marker, 'click',
+        function() {
+            info.close();
+            info.setContent(this.title);
+            info.open(gmap, this);
+        }
+    );
+}
