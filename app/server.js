7a112ea929512a11f8120500fe83e6a0e254f456
Jake Jarvis <jakejarvis@gmail.com>
2015-04-23 18:40:55 -0400

    start basic node/express files

---
+++ app/server.js
@@ -0,0 +1,16 @@
+// Initialization
+var express = require('express');
+var bodyParser = require('body-parser');
+var validator = require('validator');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mongo initialization and connect to database
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/phood';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+    db = databaseConnection;
+});
+
+app.listen(process.env.PORT || 3000);
