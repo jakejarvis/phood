7edddead28e99f5052322cc45dd69954f15f9a1d
Jake Jarvis <jakejarvis@gmail.com>
2015-04-28 14:01:04 -0400

    set up server to host static assets

--- app/server.js
+++ app/server.js
@@ -2,15 +2,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var validator = require('validator');
+var path = require('path');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// set the view engine to ejs
+// set the view engine to ejs and direct it to /views
 app.set('view engine', 'ejs');
-
-var path = require('path');
 app.set('views', path.join(__dirname, 'views/'));
+
+// use /public for static files like images, CSS, JS
+app.use(express.static(__dirname + '/public'));
 
 // Mongo initialization and connect to database
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/phood';
