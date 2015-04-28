ad2d3f7c8c5d169003f43dce81e450140b0f3611
dcroberts <dcaroberts@gmail.com>
2015-04-28 19:07:38 -0400

    export and execute twilio

--- twilio.js
+++ twilio.js
@@ -3,14 +3,17 @@ module.exports = {
 	var authToken = "5d08bb7f44240cabd1b96a73f6f30f1b";
 	var client = require('twilio')(accountSid, authToken);
 	 
-
-	// message will have the restaurant's name, a photo of from the restaurant, and a link to its page
-	client.sms.messages.create({
-	    body: "Jenny please?! Gimme another chance! I CAN DO BETTER",
-	    MediaUrl: "http://gph.is/YCpEbw",
-	    to: "+15026459166",
-	    from: "+12817466310"
-	}, function(err, sms) {
-	    process.stdout.write(sms.sid);
-	});
+	execute: function(app) {
+		app.post('/sendSMS', function(req, res) {
+			// message will have the restaurant's name, a photo of from the restaurant, and a link to its page
+			client.sms.messages.create({
+			    body: "Jenny please?! Gimme another chance! I CAN DO BETTER",
+			    MediaUrl: "http://gph.is/YCpEbw",
+			    to: "+15026459166",
+			    from: "+12817466310"
+			}, function(err, sms) {
+			    process.stdout.write(sms.sid);
+			});
+		})
+	}
 };
