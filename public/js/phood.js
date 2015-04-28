b84aa5f70a257578f005ef11ec347dd5630e7892
Kelsey Gilchrist <kgilchrist15@gmail.com>
2015-04-28 19:00:57 -0400

    rest markers

--- public/js/phood.js
+++ public/js/phood.js
@@ -42,3 +42,39 @@ function renderMap() {
         }
     );
 }
+
+function foursquare(){
+
+    $.getJSON('https://api.foursquare.com/v2/venues/search?ll='+mylat+','+mylng+'&section=food&client_id=AHETXZDGE5YWYLLM5AR13UTWC3UXETSPE54UHAOVRNPJLXIT&client_secret=SCLHL3DIHUSWIBWALLQE3TDHMEZUPCPVRV55FEN0WJRBJPU2&v=20150426', {}, function(data) {
+        for(var i=0; i < data.response.venues.length; i++) {
+            createMarker(data.response.venues[i]);
+        }
+    });
+}
+
+function createMarker(place) {
+
+    var lat = place.location.lat;
+    var lng = place.location.lng;
+    var latlng = new google.maps.LatLng(lat, lng);
+    var marker = new google.maps.Marker({
+                    position: latlng,
+                    map: gmap,
+                    title: place.name
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.close();
+        infowindow.setContent(place.name);
+        infowindow.open(gmap, this);
+    });
+
+}
+
+
+
+     // console.log(data.response.venues[0].name);
+      //id = data.response.venues[0].id;
+
+     // console.log(id);
+
+}
