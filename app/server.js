9cd83ba95b019630ba90eb0190473f8cb9ee45b5
Jake Jarvis <jakejarvis@gmail.com>
2015-04-28 13:47:50 -0400

    render homepage using template

--- app/server.js
+++ app/server.js
@@ -6,11 +6,19 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // Mongo initialization and connect to database
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/phood';
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
     db = databaseConnection;
+});
+
+// homepage 
+app.get('/', function(req, res) {
+    res.render('views/pages/index.html');
 });
 
 app.listen(process.env.PORT || 3000);
