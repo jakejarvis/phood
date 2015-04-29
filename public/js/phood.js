ac9758dfc2f6679129ab761214a938d9a2af63db
Jake Jarvis <jakejarvis@gmail.com>
2015-04-29 17:48:42 -0400

    check if we're displaying a map before executing map JS

--- public/js/phood.js
+++ public/js/phood.js
@@ -5,22 +5,26 @@ var info = new google.maps.InfoWindow;
 
 function init() {
 
-    var map_canvas = document.getElementById("map_canvas");
-    var tufts = new google.maps.LatLng(42.406484, -71.119023);
-    var options = {zoom: 15, center: tufts};
+    // only do this if we're displaying a map (#map_canvas) on the page
+    if($('#map_canvas').length) {
+        var map_canvas = document.getElementById("map_canvas");
+        var tufts = new google.maps.LatLng(42.406484, -71.119023);
+        var options = {zoom: 15, center: tufts};
 
-    gmap = new google.maps.Map(map_canvas, options);
+        gmap = new google.maps.Map(map_canvas, options);
 
 
-    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-        navigator.geolocation.getCurrentPosition(function(position) {
-            myLat = position.coords.latitude;
-            myLng = position.coords.longitude;
-            renderMap();
-        });
-    } else {
-        alert("Please try a different browser.");
+        if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+            navigator.geolocation.getCurrentPosition(function(position) {
+                myLat = position.coords.latitude;
+                myLng = position.coords.longitude;
+                renderMap();
+            });
+        } else {
+            alert("Please try a different browser.");
+        }
     }
+
     
 }
 
