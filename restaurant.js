39749556b6e867c050fc786bff2ba6846e2ac4e0
dcroberts <dcaroberts@gmail.com>
2015-04-29 15:18:54 -0400

    updating restuarant page to display name

--- restaurant.js
+++ restaurant.js
@@ -3,8 +3,8 @@ module.exports = {
         // restaurant page 
         app.get('/restaurant', function(req, res) {
             var id = req.param('id'); // get id parameter from url (/restaurant?id=xxxxxxx)
-
-            res.render('pages/restaurant', {name: "Chipotle", id: id});
+            var restaurant_name = req.param('name');
+            res.render('pages/restaurant', {name: restaurant_name, id: id});
         });
     }
 }
