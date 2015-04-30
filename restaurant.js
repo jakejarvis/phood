57aac1b22e0e149e0519796ec11e95d580629f6c
Jake Jarvis <jakejarvis@gmail.com>
2015-04-30 12:31:26 -0400

    no more crashing when restaurant has no photos

--- restaurant.js
+++ restaurant.js
@@ -22,84 +22,92 @@ module.exports = {
                 response.on('end', function() {
                     var jsonString = JSON.stringify(eval("(" + str + ")"));
                     var jsonData = JSON.parse(jsonString);
-                    var id = jsonData.data[0].id;
 
-                    var instaOptions = {
-                        host: 'api.instagram.com',
-                        path: '/v1/locations/' + id + '/media/recent?access_token=1825654502.22880a8.088578a5f9a34067ad37caf165e0ba03'
-                    };
+                    if(jsonData.data.length) {
 
-                    instaCallback = function(response) {
+                        var id = jsonData.data[0].id;
 
-                        var instastr = '';
+                        var instaOptions = {
+                            host: 'api.instagram.com',
+                            path: '/v1/locations/' + id + '/media/recent?access_token=1825654502.22880a8.088578a5f9a34067ad37caf165e0ba03'
+                        };
 
-                        //another chunk of data has been recieved, so append it to `str`
-                        response.on('data', function (chunk) {
-                            instastr += chunk;
-                        });
+                        instaCallback = function(response) {
 
-                        //the whole response has been recieved, so we just print it out here
-                        response.on('end', function () {
-                            var instaString = JSON.stringify(eval("(" + instastr + ")"));
-                            var instaData = JSON.parse(instaString);
+                            var instastr = '';
 
-                            images = [];
+                            //another chunk of data has been recieved, so append it to `str`
+                            response.on('data', function (chunk) {
+                                instastr += chunk;
+                            });
 
-                            for (var i = 0; i < instaData.data.length; i++) {
-                                if (instaData.data[i].type == "image") {
-                                    var imageURL = instaData.data[i].images.standard_resolution.url;
+                            //the whole response has been recieved, so we just print it out here
+                            response.on('end', function () {
+                                var instaString = JSON.stringify(eval("(" + instastr + ")"));
+                                var instaData = JSON.parse(instaString);
 
-                                    images.push(imageURL);
-                                }
-                            }
+                                images = [];
 
+                                for (var i = 0; i < instaData.data.length; i++) {
+                                    if (instaData.data[i].type == "image") {
+                                        var imageURL = instaData.data[i].images.standard_resolution.url;
 
-                            var foursquareOptions = {
-                                host: 'api.foursquare.com',
-                                path: '/v2/venues/' + foursquareid + '?client_id=AHETXZDGE5YWYLLM5AR13UTWC3UXETSPE54UHAOVRNPJLXIT&client_secret=SCLHL3DIHUSWIBWALLQE3TDHMEZUPCPVRV55FEN0WJRBJPU2&v=20150426'
-                            };
-
-                            foursquareCallback = function(response) {
-                                var str = '';
-
-                                //another chunk of data has been recieved, so append it to `str`
-                                response.on('data', function(chunk) {
-                                   str += chunk;
-                                });
-
-                                //the whole response has been recieved, so we just print it out here
-                                response.on('end', function () {
-
-                                    var foursquareString = JSON.stringify(eval("(" + str + ")"));
-                                    var foursquareData = JSON.parse(foursquareString);
-
-                                    address = "";
-
-
-                                    name = foursquareData.response.venue.name;
-
-                                    for(var i = 0; i < foursquareData.response.venue.location.formattedAddress.length; i++) {
-                                        address += foursquareData.response.venue.location.formattedAddress[i] + "\n";
+                                        images.push(imageURL);
                                     }
+                                }
 
-                                    res.render('pages/restaurant', {
-                                        name: name,
-                                        id: id,
-                                        address: address,
-                                        images: images
+
+                                var foursquareOptions = {
+                                    host: 'api.foursquare.com',
+                                    path: '/v2/venues/' + foursquareid + '?client_id=AHETXZDGE5YWYLLM5AR13UTWC3UXETSPE54UHAOVRNPJLXIT&client_secret=SCLHL3DIHUSWIBWALLQE3TDHMEZUPCPVRV55FEN0WJRBJPU2&v=20150426'
+                                };
+
+                                foursquareCallback = function(response) {
+                                    var str = '';
+
+                                    //another chunk of data has been recieved, so append it to `str`
+                                    response.on('data', function(chunk) {
+                                       str += chunk;
                                     });
-                                    
 
-                                });
+                                    //the whole response has been recieved, so we just print it out here
+                                    response.on('end', function () {
 
-                            }
+                                        var foursquareString = JSON.stringify(eval("(" + str + ")"));
+                                        var foursquareData = JSON.parse(foursquareString);
 
-                            http.request(foursquareOptions, foursquareCallback).end();
+                                        address = "";
 
-                        });
+
+                                        name = foursquareData.response.venue.name;
+
+                                        for(var i = 0; i < foursquareData.response.venue.location.formattedAddress.length; i++) {
+                                            address += foursquareData.response.venue.location.formattedAddress[i] + "\n";
+                                        }
+
+                                        res.render('pages/restaurant', {
+                                            name: name,
+                                            id: id,
+                                            address: address,
+                                            images: images
+                                        });
+                                        
+
+                                    });
+
+                                }
+
+                                http.request(foursquareOptions, foursquareCallback).end();
+
+                            });
+                        }
+
+                        http.request(instaOptions, instaCallback).end();
+                    } else {
+
+                        res.render('pages/nophotos');
+
                     }
-
-                    http.request(instaOptions, instaCallback).end();
                 });
             }
 
